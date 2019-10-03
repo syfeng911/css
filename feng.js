51688ba4fb44fb7d09a862d4089e7fc7fc70e2e0
syfeng911 <gfc_syfeng@hotmail.com>
2019-10-04 04:21:28 +0800

    Update feng.js

--- feng.js
+++ feng.js
@@ -1,3 +1,10 @@
+//啟動 Burger
+document.querySelector('.navbar-burger').onclick = function() {
+  document.querySelector('.navbar-burger').classList.toggle('is-active');
+  document.querySelector('.nav-menu').classList.toggle('is-active');
+}
+
+//關閉 Modale
 document.querySelectorAll('.modal').forEach(function(e){
     e.querySelectorAll('.delete, .button').forEach(function(a){
       a.onclick = function(){
@@ -6,10 +13,11 @@ document.querySelectorAll('.modal').forEach(function(e){
     });
 });
 
+//開啟 Modale
 document.querySelectorAll('.modal-link').forEach(function(e){
   var target = e.dataset.target;
   e.onclick = function(){
     document.getElementById(target).classList.add('is-active');
   }
 
-})
+});
